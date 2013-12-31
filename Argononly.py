346f7887239ede2ac4733d89c6d98d2dd7e6b1a0
geosh <sharonmariamgeorge@gmail.com>
2013-12-31 15:31:30 +0530

    gap conductance  code with helium and Argon

--- Argononly.py
+++ Argononly.py
@@ -12,13 +12,7 @@ def cal_G(p1,t1):
   d= float(2*(((gas.volume_mole/(6.023*10**23))*(3/(4*3.14)))**(1/3)))
   lamda=float((.0821*t1)/(math.sqrt(2)*math.pi*d*d*p1*6.023*10**23))
 
-  k= gas.thermal_conductivity
-  Cp= gas.cp
-  v= gas.viscosity
-  Pr= (v*Cp/k)
-  Cv= gas.cv
-  gamma= Cp/Cv
-  #alpha of Ar- .85
+  
 
   G_jump= float(2*((2-.85)/.85)*(gamma/(gamma+1))*(k/(Pr*Cp))*lamda)
   #assuming surface roughness to be 1
